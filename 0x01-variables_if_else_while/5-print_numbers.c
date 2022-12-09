d695771b848648147ba06a3fefc40948e487de70
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-09 17:46:17 +0000

    Task 5: Prints single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits of base 10 starting from 0.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int singledigits;
+
+	for (singledigits = 0; singledigits < 10; singledigits++)
+		printf("%d\n", singledigits);
+
+	return (0);
+}
