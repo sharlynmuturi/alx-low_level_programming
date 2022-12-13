c8f17521dc02bb1bbc616b23b7d853c6f39d081c
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-13 12:48:30 +0000

    Task 8: Print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ *
+ * Return: 24 hour clock line by line
+ */
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
