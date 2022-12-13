5214a34a004147520c1cfc22c05d4a8232b8841d
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-13 11:19:57 +0000

    Task 3: Checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/*
+ * _islower(int c) - checks for lowercase character
+ *
+ * Return: 1 if c is lowercase
+ * Return: 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
