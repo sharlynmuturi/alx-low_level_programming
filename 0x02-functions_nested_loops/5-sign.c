d80a39a3d9d2eb7a58a0bb950b91438e077f466f
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-13 11:44:44 +0000

    Task 5: Checks sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ * print_sign(int n) - checks sign of a number
+ *@n: number input
+ * Return: 1 and prints + if n is positive
+ *	0 and prints 0 if n is zero
+ *	-1 and prints - if n is negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
