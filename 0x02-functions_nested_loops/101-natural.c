b15ad698283f43344ad6809612efb996ca22b8f6
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-14 17:20:07 +0000

    Task 13: Prints sum of natural numbers below 1024 multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main - Prints all natural numbers multiples of 3 or 5
+ * that are below 1024
  *
  * Return: 0
  */
 int main(void)
 {
+	int num, sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3) == 0 || (num % 5) == 0)
+			sum += num;
+	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
