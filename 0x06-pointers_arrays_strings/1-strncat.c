d649d7de703ecbb0fdbaf9e1040f8df22cd1ebf5
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-21 19:38:55 +0000

    Code improved

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * _strncat - concatenates two strings
  * @dest: copy destination
  * @src: copy from
- * @n: input of max bytes to be used
+ * @n: no of elements to concatenate in
  * Return: 0
  */
 char *_strncat(char *dest, char *src, int n)
@@ -11,12 +11,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i, count;
 
 	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	for (count = 0; count < n; count++)
+	for (count = 0; src[count] != '\0' && n > 0; count++, n--, i++)
 	{
-		dest[i + count] = src[count];
-		if (src[count] == '\0')
-			count = n;
+		dest[i] = src[count];
 	}
 
 	return (dest);
