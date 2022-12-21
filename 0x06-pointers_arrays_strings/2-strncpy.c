264969ad36434ebe133c92380cc302d902b9a88e
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-21 19:01:51 +0000

    Commit 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,20 +2,20 @@
 
 /**
  * _strncpy - Entry point
- * @dest: copy destination
- * @src: copy from
- * @n: input no of characters
+ * @dest: string input
+ * @src: string input
+ * @n: input no of characters to copy over
  * Return: 0
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+		for (; n > i; i++)
+			dest[i] = '\0';
 
 	return (dest);
 }
