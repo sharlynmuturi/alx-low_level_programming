f671eb6d816a36c7171bb953a734ea16bf8a169f
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-21 17:53:58 +0000

    Second commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++);
 
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	for (j = 0; src[j] != '\0'; j++);
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
