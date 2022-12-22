98bf331c16f81304e1ad6dc5a071977efb9cbcec
sharlynmuturi <sharlynnmuturi@gmail.com>
2022-12-22 07:09:21 +0000

    Third commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,13 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++)
+		{
+		}
 
-		for (j = 0; (dest[i + j] = *src++) != '\0'; j++);
+	for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
+		{
+		}
 
 	return (dest);
 }
