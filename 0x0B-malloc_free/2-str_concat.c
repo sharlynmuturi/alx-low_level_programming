c5e232b5432c697efcb3fec9760292425b406dbe
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-01-11 15:19:02 +0000

    Second commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string type
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	return (a);
+}
+
+/**
+ * _strncat - concatenates strings with n bytes
+ * @dest: destination for concatenation
+ * @src: source of string
+ * @n: int input for size of bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destLen, a;
+
+	destLen = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[destLen + a] = src[a];
+	return (dest);
+}
+
 /**
  * str_concat - concatenates two strings.
  * @s1: string input1
@@ -12,38 +45,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int ends1, ends2;
-	int i = 0;
-	char *array;
+	char *ptr;
+	int size1, size2;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-
-	for (ends1 = 0; ends1 <= *s1; ends1++)
+	if (s1 == NULL)
 	{
+		s1 == "";
+	}
+	if (s2 == NULL)
+	{
+		s2 == "";
 	}
 
-	for (ends2 = 0; ends2 <= *s2; ends2++)
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+
+	if (ptr == NULL)
 	{
-	}
-
-	array = malloc(sizeof(char) * (ends1 + ends2 + 1));
-
-	if (array == NULL)
 		return (NULL);
-
-	while (*s1)
-	{
-		array[i] = *s1;
-		i++;
-		s1++;
 	}
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
 
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (array);
+	return (ptr);
 }
