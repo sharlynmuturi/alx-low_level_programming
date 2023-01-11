54d8a8049d038ddc48cea4bcdb6ad29f47871819
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-01-11 17:06:16 +0000

    Second commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,34 +10,32 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **array;
-	int i, n;
+	int i, j;
+	int **ptr;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	array = malloc(sizeof(int) * height);
-
-	if (array == NULL)
 	{
-		free(array);
 		return (NULL);
 	}
-
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(sizeof(int) * width);
-
-		if (array == NULL)
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
 		{
-			free(array);
+			for (j = 0; j < i; j++)
+				free(ptr[j]);
+			free(ptr);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
+		for (j = 0; j < width; j++)
 		{
-			array[i][n] = 0;
+			ptr[i][j] = 0;
 		}
 	}
-	return (array);
+	return (ptr);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,12 +13,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid != NULL || height != 0)
+	if (grid == NULL || grid == 0)
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
+		return;
 	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
