97804aa37e667bb989a6421da0baa7f0c1c8eda2
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-01-17 15:56:12 +0000

    Second commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,12 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+			return;
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
