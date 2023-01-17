07ce8ae69ba59059ea51aff382ec9a929171f7f6
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-01-17 16:08:02 +0000

    Second commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-
+		printf("Name: (nil)\n");
+	else
 		printf("Name: %s\n", d->name);
-		printf("Age: %.6f\n", d->age);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
 		printf("Owner: %s\n", d->owner);
 }
