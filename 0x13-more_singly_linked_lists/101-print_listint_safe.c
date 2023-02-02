4c8cee0baf4aeb1debe5cd8261e2d5748da5ef1c
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-02-02 07:22:24 +0000

    Advance task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: head
+ * Return: number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	if (head == NULL)
+		exit(98);
+
+	return (0);
+}
